9ef984dffe4ffa9ce6f1a632b6d2d2a2ccf80529
qsdcv <usamarupokepoke0576@gmail.com>
2019-12-11 21:36:16 +0900

    Automatic_updating_create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,32 +34,53 @@ $(function(){
                     </p>
                   </div>
                 </div>`
+      e.preventDefault();
+      var message = new FormData(this);
+      var url =  $(this).attr('action');
+      $.ajax({  
+        url: url,
+        type: 'POST',
+        data: message,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('#new_message')[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        return false
+      })  
+      .fail(function(data){
+            alert("メッセージ送信に失敗しました");
+      })
+      .always(function(data){
+        $('.form__submit').prop('disabled', false);
+      })
+    })    
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){ 
+    var last_message_id = $('.message:last').data("message-id"); 
+      $.ajax({  
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });
     }
-    return html
   }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var message = new FormData(this);
-    var url =  $(this).attr('action');
-    $.ajax({  
-      url: url,
-      type: 'POST',
-      data: message,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    })  
-    .fail(function(data){
-          alert("メッセージ送信に失敗しました");
-    })
-    .always(function(data){
-      $('.form__submit').prop('disabled', false);
-    })
-  })
+    setInterval(reloadMessages, 7000); 
+    
 })
